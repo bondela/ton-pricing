12f737ca493ad72b57ddcd141e070ea0555c1065
bondela <mikita.makarevich@gmail.com>
2024-04-16 00:48:27 +0300

    removed second currency

--- bot/handlers/posting/api.py
+++ bot/handlers/posting/api.py
@@ -2,4 +2,4 @@ from requests import get
 
 
 async def get_exchange() -> dict:
-    return get("https://tonapi.io/v2/rates?tokens=ton&currencies=ton%2Cusd%2Crub").json()
+    return get("https://tonapi.io/v2/rates?tokens=ton&currencies=ton%2Cusd").json()
