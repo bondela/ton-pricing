33b4aa3672928931c5f212c9f31cfc321c8dafe9
bondela <mikita.makarevich@gmail.com>
2024-04-16 00:49:30 +0300

    fixed paths, more beautiful output

--- bot/handlers/posting/post.py
+++ bot/handlers/posting/post.py
@@ -4,8 +4,7 @@ from aiogram import Bot
 from icecream import ic
 
 from .api import get_exchange
-from config.config_reader import config
-
+from config_reader import config
 
 
 async def scheduled_post(bot: Bot = None) -> Any:
@@ -22,13 +21,14 @@ async def scheduled_post(bot: Bot = None) -> Any:
         return
 
     ton_usd = response.get("rates", {}).get("TON", {}).get("prices", {}).get("USD", {}) or 0
-
     diff_day = response.get("rates", {}).get("TON", {}).get("diff_24h", {}).get("USD", {}) or 0
     diff_week = response.get("rates", {}).get("TON", {}).get("diff_7d", {}).get("USD", {}) or 0
     diff_month = response.get("rates", {}).get("TON", {}).get("diff_30d", {}).get("USD", {}) or 0
 
     # TODO: Use redis. Post message every 5 minutes, update message (edit) with actual information every 30 seconds.
-    await bot.send_message(chat_id=config.channel_id, text=f"<b>${round(ton_usd, 2)} 🇺🇸\n\n"
-                                                           f"Last 24h: <code>{diff_day}</code>\n"
-                                                           f"Last week: <code>{diff_week}</code>\n"
-                                                           f"Last month: <code>{diff_month}</code></b>")
+
+    await bot.send_message(chat_id=config.channel_id,
+                           text=f"<b><blockquote>🇺🇸 ${round(ton_usd, 2)} {'👹' if str(ton_usd).startswith("6.66") else ''}</blockquote>\n\n"
+                                f"Last 24h: <code>{diff_day}</code>\n"
+                                f"Last week: <code>{diff_week}</code>\n"
+                                f"Last month: <code>{diff_month}</code></b>")
