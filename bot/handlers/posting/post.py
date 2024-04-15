88222ad1d7a018ffb8030c4946d3b22994bcb164
bondela <mikita.makarevich@gmail.com>
2024-04-15 17:20:03 +0300

    removed useless rate in russian ruble

--- bot/handlers/posting/post.py
+++ bot/handlers/posting/post.py
@@ -22,14 +22,13 @@ async def scheduled_post(bot: Bot = None) -> Any:
         return
 
     ton_usd = response.get("rates", {}).get("TON", {}).get("prices", {}).get("USD", {}) or 0
-    ton_rub = response.get("rates", {}).get("TON", {}).get("prices", {}).get("RUB", {}) or 0
 
     diff_day = response.get("rates", {}).get("TON", {}).get("diff_24h", {}).get("USD", {}) or 0
     diff_week = response.get("rates", {}).get("TON", {}).get("diff_7d", {}).get("USD", {}) or 0
     diff_month = response.get("rates", {}).get("TON", {}).get("diff_30d", {}).get("USD", {}) or 0
 
     # TODO: Use redis. Post message every 5 minutes, update message (edit) with actual information every 30 seconds.
-    await bot.send_message(chat_id=config.channel_id, text=f"<b>🇺🇸 ${round(ton_usd, 2)} ~ 🇷🇺{round(ton_rub, 2)}₽\n\n"
+    await bot.send_message(chat_id=config.channel_id, text=f"<b>${round(ton_usd, 2)} 🇺🇸\n\n"
                                                            f"Last 24h: <code>{diff_day}</code>\n"
                                                            f"Last week: <code>{diff_week}</code>\n"
                                                            f"Last month: <code>{diff_month}</code></b>")
